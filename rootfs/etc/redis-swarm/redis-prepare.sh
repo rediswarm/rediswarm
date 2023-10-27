59a410f473db5e304c6168e341ebfbd53a21911e
Socheat Sok <socheatsok78@gmail.com>
2023-10-27 12:58:09 +0700

    Refactor redis & sentinel service, deployment and others

--- rootfs/etc/redis-swarm/redis-prepare.sh
+++ rootfs/etc/redis-swarm/redis-prepare.sh
@@ -1,9 +1,9 @@
 #!/command/with-contenv bash
 # vim:sw=2:ts=2:sts=2:et
 set -e
+source "/.rediswarm"
 
 ME=$(basename "$0")
-export REDIS_MASTER_PORT=${REDIS_MASTER_PORT:-6379}
 
 entrypoint_log() {
     if [ -z "${REDIS_QUIET_LOGS:-}" ]; then
@@ -11,19 +11,19 @@ entrypoint_log() {
     fi
 }
 
-REDIS_SWARM_SLOT_machine() {
-    if [ "${REDIS_SWARM_SLOT}" -eq 1 ]; then
+REDISWARM_SLOT_machine() {
+    if [ "${REDISWARM_SLOT}" -eq 1 ]; then
         entrypoint_log "$ME: Redis is running on slot 1, identify as primary!"
         export REDIS_SLAVEOF=""
     else
-        entrypoint_log "$ME: Redis is running on slot ${REDIS_SWARM_SLOT}, identify as replica!"
-        REDIS_MASTER_ADDR=""
-        for((i=1;i<=${REDIS_SWARM_SLOT_RETRY};i++)); do
+        entrypoint_log "$ME: Redis is running on slot ${REDISWARM_SLOT}, identify as replica!"
+        REDIS_PRIMARY_ADDR=""
+        for((i=1;i<=${REDISWARM_SLOT_RETRY};i++)); do
             entrypoint_log "$ME: Attempting to find master on slot ${i}"
-            REDIS_MASTER_ADDR="${REDIS_HOSTNAME_PREFIX}${i}"
-            wait-for-it.sh --quiet -t 30 "${REDIS_MASTER_ADDR}:${REDIS_MASTER_PORT}" -s -- echo "$ME: Redis primary is up at ${REDIS_MASTER_ADDR}" && break
+            REDIS_PRIMARY_ADDR="${REDIS_HOSTNAME_PREFIX}${i}"
+            wait-for-it.sh --quiet -t ${REDISWARM_SLOT_TIMEOUT} "${REDIS_PRIMARY_ADDR}:${REDIS_PRIMARY_PORT}" -s -- echo "$ME: Redis primary is up at ${REDIS_PRIMARY_ADDR}" && break
         done
-        export REDIS_SLAVEOF="replicaof ${REDIS_MASTER_ADDR} ${REDIS_MASTER_PORT}"
+        export REDIS_SLAVEOF="replicaof ${REDIS_PRIMARY_ADDR} ${REDIS_PRIMARY_PORT}"
     fi
 }
 
@@ -39,7 +39,5 @@ auto_envsubst() {
     envsubst "$defined_envs" < "$template_file" > "$config_file"
 }
 
-REDIS_SWARM_SLOT_machine
+REDISWARM_SLOT_machine
 auto_envsubst
-
-exit 0
--- rootfs/etc/redis-swarm/redis-swarm.sh
+++ rootfs/etc/redis-swarm/redis-swarm.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# vim:sw=2:ts=2:sts=2:et
+set -e
+source "/.rediswarm"
 
 cat /etc/redis-swarm/banner.txt
+
+echo "- REDISWARM_MODE=${REDISWARM_MODE}"
+echo "- REDISWARM_SLOT=${REDISWARM_SLOT}"
+echo "- REDISWARM_SLOT_RETRY=${REDISWARM_SLOT_RETRY}"
+echo "- REDISWARM_SLOT_TIMEOUT=${REDISWARM_SLOT_TIMEOUT}"
+echo ""
